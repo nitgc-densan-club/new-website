39a4895e49e8efd25c52f446ab70f553aa911259
slerk3357 <slerke399@hotmail.com>
2023-10-16 22:58:38 +0900

    headerをWeb Componentsに対応

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,33 @@
-const headerScript = () => {
-    const tocButton = document.getElementById("tocButton");
-    tocButton?.addEventListener("click",()=>{
-        tocButton.classList.toggle("active");
-        document.body.classList.toggle("toc-active");
-    });
-    //console.log(tocButton);
+fetch("/src/components/header/header.html")
+    .then((stream) => stream.text())
+    .then((text) => define(text));
 
-    const menuButton = document.getElementById("menuButton");
-    menuButton?.addEventListener("click",()=>{
-        menuButton.classList.toggle("active");
-        document.body.classList.toggle("menu-active");
-    });
-    //console.log(menuButton);
+function define(text){
+    const template = new DOMParser().parseFromString(text,"text/html").querySelector("template");
+
+    class TopHeader extends HTMLElement {
+        constructor(){
+            super();
+            const shadow = this.attachShadow({mode:"open"});
+            shadow.appendChild(template.content.cloneNode(true));
+        }
+        connectedCallback(){
+            const shadow = this.shadowRoot;
+            const tocButton = shadow.getElementById("tocButton");
+            tocButton?.addEventListener("click",()=>{
+                tocButton.classList.toggle("active");
+                document.body.classList.toggle("toc-active");
+            });
+            //console.log(tocButton);
+
+            const menuButton = shadow.getElementById("menuButton");
+            menuButton?.addEventListener("click",()=>{
+                menuButton.classList.toggle("active");
+                document.body.classList.toggle("menu-active");
+            });
+            //console.log(menuButton);
+        }
+    }
+
+    customElements.define("top-header",TopHeader);
 }
-
-headerScript();
-
-export { headerScript };
