a6b652fca9caf62185ce148fe4ca22a4da411123
slerk3357 <slerke399@hotmail.com>
2023-10-25 17:19:50 +0900

    nav-menuを動作するように修正

--- docs/components/nav-menu/nav-menu.js
+++ docs/components/nav-menu/nav-menu.js
@@ -6,12 +6,20 @@ function define_NavMenu(text){
     const template = new DOMParser().parseFromString(text,"text/html").querySelector("template");
 
     class NavMenu extends HTMLElement {
+        static observedAttributes = ["class"];
         constructor(){
             super();
             const shadow = this.attachShadow({mode:"open"});
             shadow.appendChild(template.content.cloneNode(true));
         }
         connectedCallback(){}
+        attributeChangedCallback(name, oldValue, newValue) {
+            if(newValue === "active"){
+                this.shadowRoot.getElementById("menu").classList.add("active");
+            }else{
+                this.shadowRoot.getElementById("menu").classList.remove("active");
+            }
+        }
     }
 
     customElements.define("nav-menu",NavMenu);
